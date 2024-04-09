89402d3be643939bbb3e14b850d6a580252ebb21
Phyllipe Medeiros <phyllipe@reidopitaco.com>
2024-04-09 10:22:02 -0300

    [refactor] Use thiserror to represent Statsig Error

--- src/statsig/statsig_error.rs
+++ src/statsig/statsig_error.rs
@@ -1,35 +1,15 @@
-pub struct StatsigError {
-    pub message: String,
-}
+use thiserror::Error;
 
-impl StatsigError {
-    pub fn singleton_lock_failure() -> Self {
-        StatsigError {
-            message: "Failed to acquire mutex lock on Statsig instance".to_string(),
-        }
-    }
-
-    pub fn already_initialized() -> Self {
-        StatsigError {
-            message: "Statsig is already initialized".to_string(),
-        }
-    }
-
-    pub fn instantiation_failure() -> Self {
-        StatsigError {
-            message: "Failed to create a Statsig instance".to_string(),
-        }
-    }
-
-    pub fn uninitialized() -> Self {
-        StatsigError {
-            message: "You must call and await Statsig.initialize first.".to_string(),
-        }
-    }
-
-    pub fn shutdown_failure() -> Self {
-        StatsigError {
-            message: "Was unable to gracefully shutdown the Statsig instance".to_string(),
-        }
-    }
+#[derive(Error, Debug)]
+pub enum StatsigError {
+    #[error("Failed to acquire mutex lock on Statsig instance")]
+    SingletonLockFailure,
+    #[error("Statsig is already initialized")]
+    AlreadyInitialized,
+    #[error("Failed to create a Statsig instance")]
+    InstantiationFailure,
+    #[error("You must call and await Statsig.initialize first.")]
+    Uninitialized,
+    #[error("Was unable to gracefully shutdown the Statsig instance")]
+    ShutdownFailure,
 }
